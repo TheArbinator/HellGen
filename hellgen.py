a862488eaa0fcd1f2d942366d85a74ba864c2db7
TheArbinator <datlegomann@gmail.com>
2025-02-10 16:45:50 -0500

    Added stratagems and grenades. Also added a name generator.

--- hellgen.py
+++ hellgen.py
@@ -1,22 +1,49 @@
 import json
 import random
 
-print("This project is incomplete. Not all items are implemented.")
+print("HellGen: A Helldivers 2 Loadout Generator by TheArbinator")
+print("This project is incomplete. Not all items are implemented.\n")
 
 def read_json(file_path):
     with open(file_path, 'r') as file:
         data = json.load(file)
-        file.close()
         return data
 
 
-hell_dict = read_json("data/default.json")
+default = read_json("data/default.json")
 
-primaries = hell_dict.get("primaries")
-secondaries = hell_dict.get("secondaries")
+primaries = default.get("primaries")
+secondaries = default.get("secondaries")
+throwables = default.get("throwables")
+stratagems = default.get("stratagems")
 
 primary = random.choice(primaries)
 secondary = random.choice(secondaries)
+throwable = random.choice(throwables)
+stratagem_loadout = random.sample(stratagems, 4)
 
+
+# Name generation
+# TODO: Generate a relevant name if there are many items with similar tags (eg. fire, bombs, etc.)
+
+names = read_json("data/names.json")
+
+adjectives = names.get("adjectives")
+nouns = names.get("nouns")
+suffixes = names.get("suffixes")
+suffix_probability = names.get("suffix_probability")
+
+name = "The " + random.choice(adjectives) + " " + random.choice(nouns)
+
+if random.random() < suffix_probability:
+    name += " of " + random.choice(suffixes)
+
+# Output
+
+print("-- " + name + " --")
 print("Primary: " + primary.get("name"))
 print("Secondary: " + secondary.get("name"))
+print("Throwable: " + throwable.get("name"))
+print("Stratagems: ")
+for strat in stratagem_loadout:
+    print("- " + strat.get("name"))
